f3203e51d110e4665d8937844a85c8b7807afd22
Mariam0610 <traoremariam6784@gmail.com>
2023-09-01 20:11:08 +0000

    Exercice sur node jour 1

---
+++ nodemon/main.js
@@ -0,0 +1,10 @@
+const a = 5;
+const b = 10;
+let x;
+const c = 6;
+x = a + b;
+
+console.log(x);
+
+// Affichez la somme de x + c
+console.log(`La somme de x + c est égale à ${x + c}`);
---
+++ npm-node/main.js
@@ -0,0 +1,6 @@
+const a = 5;
+const b = 10;
+
+let age = a + b;
+
+console.log(`Bonjour, vous avez ${age} ans.`);
